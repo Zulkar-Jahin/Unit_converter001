ae83f1382113bae9c08365a811408b3e301e64f4
Zulkar <sigma.zulkar.2022@gmail.com>
2024-02-12 01:19:00 +0600

    Unit converter app with a image as background.Take input and show some calculated output. Output is no correct yet . Has to change it later.

---
+++ app/src/main/java/com/example/t_001/MainActivity.java
@@ -0,0 +1,59 @@
+//This file was updated last at Feb/10/2024 06:37:46
+
+package com.example.t_001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button submit;
+    private TextView instruction;
+    private TextView result;
+    private EditText inputValue;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //object detection
+        submit = findViewById(R.id.submit);
+        instruction = findViewById(R.id.instruction);
+        inputValue = findViewById(R.id.inputValue);
+        result = findViewById(R.id.result);
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this , "Hello", Toast.LENGTH_SHORT).show();
+
+                String str = inputValue.getText().toString();
+                int val = Integer.parseInt(str);
+                double output = val/1609.344;
+
+                result.setText("The ans is: " + output );
+
+            }
+        });
+
+
+
+
+    }
+
+
+
+
+
+}
